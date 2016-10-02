0705f8c6e4a9b29d01a42e98589b020b60dd9706
mtlong29 <long.matthew29@gmail.com>
2016-10-02 13:13:43 -0400

    update

--- assets/javascript/slabtext.js
+++ assets/javascript/slabtext.js
@@ -2,6 +2,6 @@
 function slabTextHeadlines() {
     $("h1").slabText({
         // Don't slabtext the headers if the viewport is under 380px
-        "viewportBreakpoint":20
+        "viewportBreakpoint":380
     });
 };
