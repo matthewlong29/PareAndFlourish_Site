d388eda60ac920aebccd74a68685c70c82625a83
Matthew Long <long.matthew29@gmail.com>
2017-07-01 16:38:49 -0400

    User select none to navbar and added script to stop images from being draggable

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,9 +1,14 @@
 $(document).ready(function () {
-	
-//	Toggles newNavbar
-//	$('#toggle').click(function () {
-//		$(this).toggleClass('active');
-//		$('#overlay').toggleClass('open');
-//	});
-	
+
+	//	Toggles newNavbar
+	//	$('#toggle').click(function () {
+	//		$(this).toggleClass('active');
+	//		$('#overlay').toggleClass('open');
+	//	});
+
+	// Makes it so that no image is draggable
+	$("img").mousedown(function () {
+		return false;
+	});
+
 });
