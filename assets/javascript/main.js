9307e404e73246ae0714b09afd8117ce3586765a
Matthew Long <long.matthew29@gmail.com>
2017-08-15 20:52:39 -0400

    added calculations for height of banner

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,16 +1,15 @@
 $(document).ready(function () {
 
-	//	Toggles newNavbar
-	//	$('#toggle').click(function () {
-	//		$(this).toggleClass('active');
-	//		$('#overlay').toggleClass('open');
-	//	});
-
-	// Makes it so that no image is draggable
-	$("img").mousedown(function () {
-		return false;
-	});
+  // Makes it so that no image is draggable
+  $("img").mousedown(function () {
+      return false;
+  });
   
+  // Call loadMore function
   $(".loadMore").click(loadMorePosts);
+  
+  // Calculate height of banner image minus navigation on homepage
+//  var navHeight = $(".nav-container").height();
+//  $("#banner").css("min-height", "calc(100vh -" + navHeight + ")");
   
 });
