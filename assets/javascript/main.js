6e81c1d68ce33be5921e0a4c8fb94e123b812cbc
Matthew Long <long.matthew29@gmail.com>
2017-08-17 17:30:39 -0400

    ..

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -6,10 +6,12 @@ $(document).ready(function () {
   });
   
   // Call loadMore function
-  $(".loadMore").click(loadMorePosts);
+//  $(".loadMore").click(loadMorePosts);
   
   // Calculate height of banner image minus navigation on homepage
 //  var navHeight = $(".nav-container").height();
 //  $("#banner").css("min-height", "calc(100vh -" + navHeight + ")");
+  
+//  $("li[data-reactid='.0.1.1.0.$282510-box']").css("border", "1px solid #DACEE8");
   
 });
