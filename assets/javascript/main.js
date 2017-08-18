dfe2d2a510dcc135d7f28a54bd3d1b34e931bc69
Matthew Long <long.matthew29@gmail.com>
2017-08-18 17:14:35 -0400

    used jquery to calculate the height of headers so that space wont vanish when navbar is fixed

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -2,16 +2,30 @@ $(document).ready(function () {
 
   // Makes it so that no image is draggable
   $("img").mousedown(function () {
-      return false;
+    return false;
   });
-  
+
   // Call loadMore function
-//  $(".loadMore").click(loadMorePosts);
-  
+  //  $(".loadMore").click(loadMorePosts);
+
   // Calculate height of banner image minus navigation on homepage
-//  var navHeight = $(".nav-container").height();
-//  $("#banner").css("min-height", "calc(100vh -" + navHeight + ")");
-  
-//  $("li[data-reactid='.0.1.1.0.$282510-box']").css("border", "1px solid #DACEE8");
-  
+  function headerHeight() {
+    // Get height of navigation container, banner (landing page), and feature images (sub pages)
+    var $navHeight = $(".nav-container").height();
+    var $bannerHeight = $("#banner").height();
+    var $featureImageHeight = $("#feature-image").height();
+    // Set height of banner (landing page)
+    $("#banner").css("min-height", "calc(100vh - " + $navHeight + "px)");
+    // Set height of header tags (landing page and sub pages)
+    $("#landingPageHeader").css("min-height", "calc(100vh)");
+    $("#subPageHeader").css("min-height", "calc(" + $navHeight + "px + " + $featureImageHeight + "px)");
+  }
+
+  // Set heights of header when window is resized
+  $(window).resize(function () {
+    headerHeight();
+  });
+
+  headerHeight();
+
 });
