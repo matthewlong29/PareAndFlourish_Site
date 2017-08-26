d2afe1f84835de748c53e073c3c2cdb3569d36ca
Matthew Long <long.matthew29@gmail.com>
2017-08-25 20:04:01 -0400

    began working on footer reveal

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -21,11 +21,55 @@ $(document).ready(function () {
     $("#subPageHeader").css("min-height", "calc(" + $navHeight + "px + " + $featureImageHeight + "px)");
   }
 
-  // Set heights of header when window is resized
+  // Add email to footer
+  var emailX = 'contact';
+  var emailY = 'pareandflourish';
+  $('.footerContact .email').html('Email: <a href="mailto:' + emailX + '@' + emailY + '.com">' + emailX + '@' + emailY + '.com</a>');
+
+  // Footer reveal effect
+//  function footerReveal() {
+//
+//    var $footerHeight = $('footer').outerHeight(true);
+//    var $footerPrimaryHeight = $('#footerPrimary').outerHeight(true);
+//    var $colophonHeight = $('#colophon').outerHeight(true);
+//
+//    $('footer').css({
+//      'z-index': '0',
+//      'height': $footerHeight
+//    });
+//
+//    $('#footerPrimary').css({
+//      'z-index': '1',
+//      'height': $footerPrimaryHeight,
+//      'background': '#fdfdfd'
+//    });
+//
+//    $('#colophon').css({
+//      'z-index': '-1',
+//      'height': $colophonHeight,
+//      'position': 'fixed',
+//      'bottom': '0',
+//      'width': '100%'
+//    });
+//
+//    console.log('footer height: ' + $footerHeight);
+//    console.log('footer primary height: ' + $footerPrimaryHeight);
+//    console.log('colophon height: ' + $colophonHeight);
+//
+//    console.log('total check: ' + ($colophonHeight + $footerPrimaryHeight));
+//
+//  }
+
+  // Call headerHeight function
+  headerHeight();
+
+  // Call footerReveal function
+//  footerReveal();
+
+  // Call functions when window is resized
   $(window).resize(function () {
     headerHeight();
+//    footerReveal();
   });
-
-  headerHeight();
 
 });
