8736deff6efaf33619e589649afed518f9f0384d
Matthew Long <long.matthew29@gmail.com>
2017-10-01 22:30:45 -0400

    added event 2 pics

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -291,21 +291,23 @@ jQuery(document).ready(function () {
   ********************************************************/
 
   const eventImage = {
-    1: 'https://raw.githubusercontent.com/mtlong29/images/master/stock/1.jpg',
-    2: 'https://raw.githubusercontent.com/mtlong29/images/master/stock/2.jpg',
-    3: 'https://raw.githubusercontent.com/mtlong29/images/master/stock/3.jpg',
-    4: 'https://raw.githubusercontent.com/mtlong29/images/master/stock/4.jpg',
-    5: 'https://raw.githubusercontent.com/mtlong29/images/master/stock/5.jpg',
-    6: 'https://raw.githubusercontent.com/mtlong29/images/master/stock/6.jpg',
-    7: 'https://raw.githubusercontent.com/mtlong29/images/master/stock/7.jpg',
-    8: 'https://raw.githubusercontent.com/mtlong29/images/master/stock/8.jpg',
-    9: 'https://raw.githubusercontent.com/mtlong29/images/master/stock/9.jpg',
-    10: 'https://raw.githubusercontent.com/mtlong29/images/master/stock/10.jpg'
+    1: 'http://www.pareandflourish.com/images/events/event1-pic1.jpg',
+    2: 'http://www.pareandflourish.com/images/events/event1-pic2.jpg',
+    3: 'http://www.pareandflourish.com/images/events/event1-pic3.jpg',
+    4: 'http://www.pareandflourish.com/images/events/event1-pic4.jpg',
+    5: 'http://www.pareandflourish.com/images/events/event1-pic5.jpg',
+    6: 'http://www.pareandflourish.com/images/events/event1-pic6.jpg',
+    7: 'http://www.pareandflourish.com/images/events/event1-pic7.jpg',
+    8: 'http://www.pareandflourish.com/images/events/event2-pic1.jpg',
+    9: 'http://www.pareandflourish.com/images/events/event2-pic2.jpg',
+    10: 'http://www.pareandflourish.com/images/events/event2-pic3.jpg',
+    11: 'http://www.pareandflourish.com/images/events/event2-pic4.jpg',
+    12: 'http://www.pareandflourish.com/images/events/event2-pic5.jpg'
   }
     
   let eventImages = '';
   
-  for (let i = 1; i < 10; i++) {
+  for (let i = 1; i < 12; i++) {
     eventImages += '<div class="eventImage" style="background: url(' + eventImage[i] + ')"></div>';
   }
   
