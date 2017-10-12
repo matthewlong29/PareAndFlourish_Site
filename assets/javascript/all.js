0a48e86b9ad67b6256e77d4d8098552aa07fa130
Matthew Long <long.matthew29@gmail.com>
2017-10-12 01:36:03 -0400

    ..

--- assets/javascript/all.js
+++ assets/javascript/all.js
@@ -440,3 +440,87 @@ client side search
     }
   }
 })();
+
+$(document).ready(function() {
+    
+  // sign up modal constants
+  const $signUpModal = $("#signUpModal");
+  const $signUpModalButton = $(".join");
+  const $signUpClose = $(".signUpClose");
+  const $signUpSubmit = $("#mc-embedded-subscribe");
+
+  // thank you modal constants
+  const $thankYouModal = $("#thankYouModal");
+  const $thankYouClose = $(".thankYouClose");
+
+  // dark overlay
+  const $darkOverlay = $('.darkness');
+  
+  // function for closing modal and return background color to transparent
+  const closeModal = modal => {
+    $("body").css({
+      background: "transparent",
+      overflow: "auto"
+    });
+    $("html").css('overflow','auto');
+    modal.hide();
+    $darkOverlay.hide();
+    stopBodyScrolling(false);    
+  }
+
+  // function for opening modal and setting dark overlays display to flex
+  const openModal = modal => {
+    $darkOverlay.css({
+      'display': 'flex',
+      'justify-content': 'center'
+    });
+    $("html").css('overflow','hidden');
+    $("body").css('overflow','hidden');
+    modal.fadeIn(100);
+    stopBodyScrolling(true);    
+  }
+
+  // function to stop scrolling (necessary for ios)
+  const stopBodyScrolling = bool => {
+    if (bool === true) {
+      document.body.addEventListener("touchmove", freezeVp, false);
+    } else {
+      document.body.removeEventListener("touchmove", freezeVp, false);
+    }
+  }
+  const freezeVp = e => {
+    e.preventDefault();
+  };
+
+  // open signup modal after 20 seconds
+    // use local storage to only show sometimes
+  // setInterval(() => {
+  //   openModal($signUpModal)
+  // }, 20000);
+  
+  // click event to open sign up modal
+  $signUpModalButton.on("click", () => {
+    openModal($signUpModal);
+  });
+
+  // click event for close button on sign up modal
+  $signUpClose.on("click", () => closeModal($signUpModal));
+  
+  // click event for close button on thank you up modal
+  $thankYouClose.on("click", () => closeModal($thankYouModal));
+
+  // click event for submit button on sign up modal
+  $signUpSubmit.on("click", e => {
+    $signUpModal.hide();
+    $thankYouModal.fadeIn(100);
+  });
+  
+  // close modals on escape key
+  $(document).on('keydown', e => {
+    if (e.which === 27) {
+      closeModal($signUpModal);
+      closeModal($thankYouModal);
+    }
+  });
+
+});
