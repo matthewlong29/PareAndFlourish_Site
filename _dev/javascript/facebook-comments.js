22dadc092be27f132e6f293240c74908649e0665
Matthew Long <long.matthew29@gmail.com>
2017-11-05 22:28:41 -0500

    updated facebook comments

--- _dev/javascript/facebook-comments.js
+++ _dev/javascript/facebook-comments.js
@@ -4,12 +4,11 @@ $(document).ready(function () {
   facebook comments
   ********************************************************/
 
-  (function (d, s, id) {
+  (function(d, s, id) {
     var js, fjs = d.getElementsByTagName(s)[0];
     if (d.getElementById(id)) return;
-    js = d.createElement(s);
-    js.id = id;
-    js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.8";
+    js = d.createElement(s); js.id = id;
+    js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.10&appId=174295113036712';
     fjs.parentNode.insertBefore(js, fjs);
   }(document, 'script', 'facebook-jssdk'));
 
